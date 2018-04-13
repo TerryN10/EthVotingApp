1f4fa647b18e2a7f18b8ca154ffcc4014084b2ca
TerryN10 <terrence.nwokeji@gmail.com>
2018-04-13 05:39:35 +0100

    Add files via upload
    Added Voting functionality

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -9,16 +9,30 @@ contract Election {
     }
 
     mapping(uint => Candidate) public candidates;
+
+    //once a voter in the mapping has been set to true, do not allow them to vote again
+    mapping(address => bool) public voters;
     uint public candidatesCount;
     
     function Election() public {
-        addCandidate("Candidate1");
-        addCandidate("Candidate2");
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
 
     function addCandidate(string _name){
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateID) public {
+
+        require(voters[msg.sender] == false);
+        require(_candidateID >0 && _candidateID <= candidatesCount);
+
+        voters[msg.sender] = true;
+        candidates[_candidateID].voteCount++;
+
+        //record voter has voted
     }
 
 }
